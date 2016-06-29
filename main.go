67c89cc76e3f6cc141e6787af0ead666db3c5466
Guillaume J. Charmes <gcharmes@leaf.ag>
2016-06-29 09:58:38 -0500

    Add MACD indicator
    Signed-off-by: Guillaume J. Charmes <gcharmes@leaf.ag>

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"sort"
+	"text/tabwriter"
 	"time"
 
 	"github.com/gofinance/ib"
@@ -66,6 +68,15 @@ type Tick struct {
 // Ticks is a list of ticks.
 type Ticks []Tick
 
+// CloseValues returns a slice of the ticks close value.
+func (t Ticks) CloseValues() []float64 {
+	ret := make([]float64, len(t))
+	for i, tick := range t {
+		ret[i] = tick.Close
+	}
+	return ret
+}
+
 // Len returns the length of the slice.
 // Implements sort.Interface.
 func (t Ticks) Len() int {
@@ -85,7 +96,7 @@ func (t Ticks) Swap(i, j int) {
 }
 
 // SimpleMovingAverage computes the SMA for the given span.
-func (t Ticks) SimpleMovingAverage(offset, period int) float64 {
+func SimpleMovingAverage(ticks []float64, offset, period int) float64 {
 	if period == 0 {
 		panic(fmt.Errorf("can't average on 0 elements"))
 	}
@@ -94,52 +105,137 @@ func (t Ticks) SimpleMovingAverage(offset, period int) float64 {
 	}
 	var sum float64
 	for i := (offset + 1) - period; i <= offset; i++ {
-		sum += t[i].Close
+		sum += ticks[i]
 	}
 	return sum / float64(period)
 }
 
-// ExponentialMovingAverage computes the EMA.
-func (t Ticks) ExponentialMovingAverage(offset, period int) float64 {
-	multiplier := float64(2 / (float64(period) + 1))
+// EMA processes the Exponential Moving Average.
+type EMA struct {
+	ticks      []float64 // Tick list.
+	emas       []float64 // EMA line.
+	multiplier float64   // Multiplier (smoothing) constant.
+	period     int       // Period for the given EMA.
+}
 
-	if len(t) < offset {
+// NewEMA instantiates a new EMA.
+func NewEMA(ticks []float64, period int) EMA {
+	return EMA{
+		ticks:      ticks,
+		emas:       make([]float64, len(ticks)),
+		multiplier: 2. / float64(period+1),
+		period:     period,
+	}
+}
+
+// Compute computes the EMA at the given offset.
+func (ema *EMA) Compute(offset int) float64 {
+	if len(ema.ticks) < offset {
 		panic(fmt.Errorf("EMA out of bounds"))
 	}
+	var ret float64
+
 	// If not enough points, return 0.
-	if (offset+1)-period < 0 {
+	if (offset+1)-ema.period < 0 {
 		return 0.
-	} else if offset+1 == period { // If len == period, then return SMA.
-		return t.SimpleMovingAverage(offset, 10)
+	} else if offset+1 == ema.period { // If len == period, then return SMA.
+		ret = SimpleMovingAverage(ema.ticks, offset, ema.period)
+	} else {
+		ret = ema.multiplier*(ema.ticks[offset]-ema.emas[offset-1]) + ema.emas[offset-1]
 	}
-	return multiplier*(t[offset].Close-t.ExponentialMovingAverage(offset-1, period)) + t.ExponentialMovingAverage(offset-1, period)
+	ema.emas[offset] = ret
+	return ret
+}
+
+// ComputeAll computes the EMA of all the known entries.
+func (ema *EMA) ComputeAll() []float64 {
+	ret := make([]float64, len(ema.ticks))
+	for i := range ema.ticks {
+		ret[i] = ema.Compute(i)
+	}
+	return ret
+}
+
+// Append appends the given tick to the list and process the EMA of the new offest.
+func (ema *EMA) Append(tick float64) float64 {
+	ema.ticks = append(ema.ticks, tick)
+	ema.emas = append(ema.emas, 0.)
+	return ema.Compute(len(ema.ticks) - 1)
+}
+
+// MACD processes the Moving Average Convergence Divergence indicator.
+type MACD struct {
+	ticks      []float64
+	macdLine   []float64
+	line1      EMA
+	line2      EMA
+	signalLine EMA
+	histogram  []float64
+}
+
+// NewMACD instantiate a new MACD processor.
+func NewMACD(ticks []float64, period1, period2, signalPeriod int) MACD {
+	ema1 := NewEMA(ticks, period1)
+	emaLine1 := ema1.ComputeAll()
+	ema2 := NewEMA(ticks, period2)
+	emaLine2 := ema2.ComputeAll()
+
+	macdLine := make([]float64, len(ticks))
+	for i := range macdLine {
+		macdLine[i] = emaLine1[i] - emaLine2[i]
+	}
+
+	signalEMA := NewEMA(macdLine, signalPeriod)
+	signalLine := signalEMA.ComputeAll()
+
+	histogram := make([]float64, len(ticks))
+	for i := range macdLine {
+		histogram[i] = macdLine[i] - signalLine[i]
+	}
+
+	return MACD{
+		ticks:      ticks,
+		macdLine:   macdLine,
+		line1:      ema1,
+		line2:      ema2,
+		signalLine: signalEMA,
+		histogram:  histogram,
+	}
+}
+
+// Append appends a new tick to the macd processor.
+func (macd *MACD) Append(tick float64) {
+	macd.ticks = append(macd.ticks, tick)
+	ema1 := macd.line1.Append(tick)
+	ema2 := macd.line2.Append(tick)
+	macd.macdLine = append(macd.macdLine, ema1-ema2)
+	signal := macd.signalLine.Append(ema1 - ema2)
+	macd.histogram = append(macd.histogram, (ema1-ema2)-signal)
 }
 
 func main() {
-	tt := Ticks{
-		{Close: 22.27},
-		{Close: 22.19},
-		{Close: 22.08},
-		{Close: 22.17},
-		{Close: 22.18},
-		{Close: 22.13},
-		{Close: 22.23},
-		{Close: 22.43},
-		{Close: 22.24},
-		{Close: 22.29},
-		{Close: 22.15},
-		{Close: 22.39},
-		{Close: 22.38},
-		{Close: 22.61},
-		{Close: 23.36},
-		{Close: 24.05},
-		{Close: 23.75},
+	macd := NewMACD(nil, 12, 26, 9)
+
+	buf, err := ioutil.ReadFile("b.json")
+	if err != nil {
+		log.Fatalf("Error loading json file: %s\n", err)
 	}
+	tt := Ticks{}
+	if err := json.Unmarshal(buf, &tt); err != nil {
+		log.Fatalf("Error parsing json file: %s\n", err)
+	}
+	w := tabwriter.NewWriter(os.Stdout, 4, 4, 4, ' ', 0)
+	fmt.Fprintf(w, "idx\tvalue\tMACD\tSignal\tHist\n")
 	for i, t := range tt {
-		ema := tt.ExponentialMovingAverage(i, 10)
-		sma := tt.SimpleMovingAverage(i, 10)
-		fmt.Printf("%d\t%f\t%.2f\t%.4f\t%.2f\n", i+1, t.Close, sma, 0.1818, ema)
+		macd.Append(t.Close)
+		if i < 26+9 {
+			continue
+		}
+		fmt.Fprintf(w, "%d\t%f\t%.2f\t%.2f\t%.2f\n", i+1, t.Close, macd.macdLine[i], macd.signalLine.emas[i], macd.histogram[i])
+
 	}
+	fmt.Fprintf(w, "\n")
+	w.Flush()
 }
 
 func main3() {
